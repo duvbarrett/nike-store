bc4ce5eb0e67d06c0e9d25e7865893c8064e40d1
Duval Barrett <97458251+duvalbarrett@users.noreply.github.com>
2023-02-08 19:44:42 -0500

    updated

---
+++ .history/src/components/Cart_20230204231302.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import CartItem from './cart/CartItem'
+import CartCount from './cart/CartCount'
+import CartEmpty from './cart/CartEmpty'
+
+const Cart = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Cart
---
+++ .history/src/components/cart/CartCount_20230202221556.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CartCount = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CartCount
